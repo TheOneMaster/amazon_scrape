3aee520f23c8bc623bacf2e87d38aad3846a148b
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-01-10 13:44:03 +0530

    Added typing

--- scrape/request.py
+++ scrape/request.py
@@ -1,15 +1,13 @@
-from typing import Literal, List, Dict
-
-import requests
-from bs4 import BeautifulSoup
-
-from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
-import tqdm
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict, List, Literal, Optional, Type, TypedDict
 
 import pandas as pd
-import csv
+import requests
+import tqdm
+from bs4 import BeautifulSoup
 
-from scrape.webpageScrape import AmazonProductData, IHerbProductData
+from scrape.webpageScrape import (AmazonProductData, IHerbProductData, P,
+                                  ProductData)
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
@@ -20,41 +18,46 @@ HEADERS = {
     'Upgrade-Insecure-Requests': '1'
 }
 
-WEBSITE_TO_URL = {
-    "Amazon": "https://www.amazon.com",
-    "IHerb": "https://www.iherb.com"
+
+WEB_DATA = TypedDict("WEBSITE DATA", {
+    "url": str,
+    "class": Type[ProductData]
+})
+
+WEBSITE_DATA: Dict[str, WEB_DATA] = {
+    "Amazon": {
+        "url": "https://www.amazon.com",
+        "class": AmazonProductData
+    },
+    "IHerb": {
+        "url": "https://www.iherb.com",
+        "class": IHerbProductData
+    }
 }
 
-WEBSITE_BASE_TO_CLASS = {
-    "Amazon": AmazonProductData,
-    "IHerb": IHerbProductData,
-}
 
-def createSearchLink(website: Literal["Amazon", "IHerb"], search_term: str, page: int) -> str:
+def createSearchLink(base_url: str, search_term: str, page: int) -> str:
 
-    base_url = WEBSITE_TO_URL.get(website, None)
-
-
-    if website == "Amazon":
+    if base_url == WEBSITE_DATA["Amazon"]['url']:
 
         return f"{base_url}/s?k={search_term}&page={page}"
 
-    elif website == "IHerb":
+    elif base_url == WEBSITE_DATA["IHerb"]['url']:
 
         raise NotImplementedError
-    
+
     else:
         raise ValueError("Website not supported")
 
 
 # Helper functions
 def getLinksFromSearch(soup: BeautifulSoup, base_url: str) -> List[str]:
-    
+
     links = soup.find_all("a", attrs={
         "class": "a-link-normal s-no-outline"
     })
 
-    ret_links = []
+    ret_links: List[str] = []
 
     for link in links:
 
@@ -79,85 +82,94 @@ def getLinksFromSearch(soup: BeautifulSoup, base_url: str) -> List[str]:
 
     unique_links = pd.unique(ret_links)
 
-    return unique_links
+    return unique_links.tolist()
+
 
 def loadLinksFromSearch(links: list[str], headers: dict) -> Dict[str, requests.Response]:
-    
+
     url_to_response = {}
-    
+
     with ThreadPoolExecutor() as executor:
-        
-        futures_to_url = {executor.submit(loadSessionUrl, url, headers): url for url in links}
-        
+
+        futures_to_url = {executor.submit(
+            loadSessionUrl, url, headers): url for url in links}
+
         for future in tqdm.tqdm(futures_to_url, desc="URL requests", unit="links"):
-            
+
             url = futures_to_url[future]
             response = future.result()
-            
+
             url_to_response[url] = response
-            
+
     return url_to_response
 
+
 def loadSessionUrl(url: str, headers: dict) -> requests.Response:
-    
+
     return requests.get(url, headers=headers)
 
-# Try to make this work
-def multiprocessedParse(product_dict: dict, productType: str) -> List[AmazonProductData|IHerbProductData]:
-    
-    with ProcessPoolExecutor() as executor:
-        
-        products = []
-        product_list = [executor.submit(lambda url, response: AmazonProductData(response, url, productType)._details) for url, response in product_dict.items()]
-        
-        for prod in product_list:
-            products.append(prod.result())
-        
-        return products
-
 # Main function to get data
-def getDataFromSearch(website: Literal["Amazon", "IHerb"], search_term: str, headers=None, page=1, output_file=False, as_df=True, raw=False, maxProducts: int=None) -> pd.DataFrame|List[AmazonProductData]:
 
-    search_url = createSearchLink(website, search_term, page)
-    
+
+def getDataFromSearch(website: Literal["Amazon", "IHerb"], search_term: str, headers=None, page=1, maxProducts: Optional[int] = None) -> List[ProductData]:
+
+    website_data = WEBSITE_DATA[website]
+    base_url = website_data['url']
+    WebsiteClass = website_data['class']
+
+    search_url = createSearchLink(base_url, search_term, page)
+
     search_headers = HEADERS.copy()
-    
+
     if isinstance(headers, dict):
         search_headers.update(headers)
-    
+
     search_page = requests.get(search_url, headers=search_headers)
     search_soup = BeautifulSoup(search_page.content, "lxml")
-    
-    base_url = WEBSITE_TO_URL[website]
+
     links = getLinksFromSearch(search_soup, base_url)
 
     if maxProducts is not None:
         links = links[:maxProducts]
-    
+
     url_to_response = loadLinksFromSearch(links, search_headers)
-    
-    if raw:
-        return url_to_response
-            
-    product_data_list = [AmazonProductData(response, url, search_term) for url, response in tqdm.tqdm(url_to_response.items(), desc="Processing webpages", unit="webpages")]
-    
-    if as_df:
-        product_data = [product.to_list() for product in product_data_list]
-        columns = product_data_list[0].columns
-        
-        product_df = pd.DataFrame(product_data, columns=columns)
 
-        # unitPrice and unitType manipulation
-        product_df['pricePerCount'] = product_df[product_df["unitType"]=='Count']['unitPrice']
-        product_df['pricePerOz'] = product_df[product_df['pricePerCount'].isnull()]['unitPrice']
-        
-        if output_file:
-            product_df.to_csv("output.csv", index=False, mode="w", encoding="utf-8",
-                            quotechar="|", quoting=csv.QUOTE_MINIMAL, sep=";")
-        
+    product_data_list = [WebsiteClass(response, url, search_term) for url, response in tqdm.tqdm(
+        url_to_response.items(), desc="Processing webpages", unit="webpages")]
 
-        product_df = product_df.drop(columns=['unitPrice', 'unitType'])
-
-        return product_df
-        
     return product_data_list
+
+
+def convertDataToDataFrame(dataArray: List[P]) -> pd.DataFrame:
+
+    product_information_array = [product.to_list() for product in dataArray]
+    columns = dataArray[0].columns
+
+    data_df = pd.DataFrame(product_information_array, columns=columns)
+
+    return data_df
+
+
+def dfPriceManipulation(df: pd.DataFrame) -> pd.DataFrame:
+
+    df_modified = df.drop(columns=['unitPrice', 'unitType'])
+
+    count_type_index = df[df['unitType'] == 'Count']
+
+    df_modified['pricePerCount'] = df[count_type_index]['unitPrice']
+    df_modified["pricePerOz"] = df[count_type_index.isnull()]['unitPrice']
+
+    return df_modified
+
+
+def search(website: Literal['Amazon', 'IHerb'], search_term: str, page=1, maxProducts: Optional[int] = None, **kwargs) -> pd.DataFrame:
+
+    headers = kwargs.get("header", None)
+
+    data = getDataFromSearch(
+        website, search_term, page=page, maxProducts=maxProducts, headers=headers)
+
+    df = convertDataToDataFrame(data)
+    df = dfPriceManipulation(df)
+
+    return df
