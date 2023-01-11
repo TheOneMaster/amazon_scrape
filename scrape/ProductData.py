1cc8581ab4e5b084f507dd741654899a1ad721f5
Nayan <25909677+TheOneMaster@users.noreply.github.com>
2023-01-11 18:05:18 +0530

    Basically finished
    - Much slower than requests
    - More reliable (probably)

--- scrape/ProductData.py
+++ scrape/ProductData.py
@@ -1,27 +1,43 @@
+from typing import Optional, Dict, List
+
+import pandas as pd
+
 class ProductData:
-    
+
     __slots__ = ("source", "url", "title", "brand", "productType", "price", "unitPrice", "unitType", "asin",
                  "rating", "numRatings", "rank", "formFactor", "firstAvailable", "uses", "ingredient", "origin")
 
     def __init__(self, source: str, url: str, productType: str, **kwargs) -> None:
-        
+
         # Set all values to None
         for slot in self.__slots__:
             setattr(self, slot, None)
-        
+
         self.source = source
         self.url = url
         self.productType = productType
-        
+
         # Add any other arguments to the data (if any applicable)
         for kwarg in kwargs:
             if kwarg in self.__slots__:
                 setattr(self, kwarg, kwargs[kwarg])
-    
-    def to_dict(self) -> dict: 
-        ret_dict = {}
-        
-        for slot in self.__slots__:
-            ret_dict[slot] = getattr(self, slot)
+
+    def to_dict(self) -> Dict[str, Optional[str|int|float]]:
+        ret_dict = {slot: getattr(self, slot) for slot in self.__slots__}
 
         return ret_dict
+
+    def to_list(self) -> List[Optional[str|int|float]]:
+        ret_list = [ getattr(self, slot) for slot in self.__slots__ ]
+        
+        return ret_list
+
+
+def convertDataToDataframe(data: List[ProductData]) -> pd.DataFrame:
+    
+    data_list = [prod.to_list() for prod in data]
+    columns = data[0].__slots__
+    
+    df = pd.DataFrame(data_list, columns=columns)
+    
+    return df
--- scrape/selenium_scrape.py
+++ scrape/selenium_scrape.py
@@ -1,4 +1,4 @@
-from typing import Type, overload, Dict, Any
+from typing import Type, overload, Dict, Any, Optional, List
 from types import TracebackType
 
 from selenium import webdriver
@@ -6,18 +6,20 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 import re
+import numpy as np
+import tqdm
 
-from ProductData import ProductData
+from ProductData import ProductData, convertDataToDataframe
 
 
-def updateAttributes(new_data: Dict[str, Any], current_data: ProductData):
+def updateAttributes(new_data: Dict[str, Any], current_data: ProductData) -> None:
     """Update the attributes of the current data with the keys from the new data"""
     for key, value in new_data.items():
         if key in current_data.__slots__:
             setattr(current_data, key, value)
-        
 
 
 class AmazonWebInstance:
@@ -36,14 +38,24 @@ class AmazonWebInstance:
         "Country of Origin": "origin"
     }
     
+    BASE_URL = "https://www.amazon.com"
+    
+    
     def __init__(self, headless=False) -> None:
         
         self.options = Options()
         self.options.headless = headless
         
-        self.driver = webdriver.Firefox(options=self.options)
+        self.options.set_preference("permissions.default.stylesheet", 2)
+        # self.options.set_preference("permissions.default.image", 2)
+        # profile = webdriver.FirefoxProfile()
+        # profile.add_extension("ublock_origin-1.46.0.xpi")
         
-        amazon_url = "https://www.amazon.com"
+        self.driver = webdriver.Firefox(options=self.options)
+        self.driver.install_addon("ext/ublock_origin-1.46.0.xpi", temporary=True)
+        # self
+        
+        amazon_url = "https://www.amazon.com/ref=nav_bb_logo"
         
         self.driver.get(amazon_url)
     
@@ -53,6 +65,8 @@ class AmazonWebInstance:
 
         self.driver.find_element(By.ID, "GLUXZipUpdateInput").send_keys("10001")
         self.driver.find_element(By.ID, "GLUXZipUpdate").click()
+        
+        
         
     def __enter__(self) -> 'AmazonWebInstance':
         return self
@@ -67,6 +81,7 @@ class AmazonWebInstance:
         
     def __exit__(self, type: Type[BaseException]|None, value: BaseException|None, traceback: TracebackType|None) -> None:
         self.driver.quit()
+    
     
     def parsePage(self, page: str, prodType: str) -> ProductData:
         
@@ -92,6 +107,9 @@ class AmazonWebInstance:
         if productData.brand is None:
             productData.brand = prod_details.get("manufacturer", unavailable_str)
             
+        ratings_data = self._parseRatings_()
+        updateAttributes(ratings_data, productData)
+            
         # Important information parsing
         important_info = self._parseImportantInfo_()
         updateAttributes(important_info, productData)
@@ -103,9 +121,9 @@ class AmazonWebInstance:
             price_data = {}
             
             # Current Price
-            core_price = self.driver.find_element(By.ID, "corePrice_desktop")
-            price_el = core_price.find_element(By.CSS_SELECTOR, ".a-price.a-text-price")
-            price = price_el.text.strip()
+            core_price = self.driver.find_element(By.CSS_SELECTOR, "div[id*='corePrice']")
+            price_el = core_price.find_element(By.CSS_SELECTOR, "span[class*='a-price']")
+            price = re.sub(r"\s", ".", price_el.text)
             
             # Unit Price
             unitPrice_span = price_el.find_element(By.XPATH, "following-sibling::*[1]")
@@ -120,7 +138,7 @@ class AmazonWebInstance:
             
             return price_data
     
-        except AttributeError:
+        except:
             return {}
         
     def _parseProductOverviewTable_(self) -> Dict[str, str]:
@@ -139,7 +157,7 @@ class AmazonWebInstance:
             
             return values
         
-        except AttributeError:
+        except:
             return {}
         
     def _parseProductDetails_(self) -> Dict[str, str|int]:
@@ -154,7 +172,7 @@ class AmazonWebInstance:
             for detail in details_list:
                 keyEl, valueEl = detail.find_element(By.TAG_NAME, "span").find_elements(By.XPATH, "*")
                 
-                key = keyEl.text[:-2]
+                key = keyEl.text.replace(":", "").strip()
                 
                 if key in self.PRODUCT_DETAILS_MAP:
                     attr = self.PRODUCT_DETAILS_MAP[key]
@@ -174,23 +192,42 @@ class AmazonWebInstance:
             
                 details_dict['rank'] = rank
 
-            # Ratings parse
-            avgRatingEl = self.driver.find_element(By.ID, "acrPopover")
-            avgRating = avgRatingEl.get_attribute("title").split()[0]
-            avgRating = float(avgRating)
-
-            numRatingsEl = self.driver.find_element(By.ID, "acrCustomerReviewText")
-            numRatings_str = numRatingsEl.text
-            numRatings_str = numRatings_str.split()[0]
-            numRatings_str = numRatings_str.replace(",", "")
-            numRatings = int(numRatings_str)
-            
-            details_dict["rating"] = avgRating
-            details_dict['numRatings'] = numRatings
-
             return details_dict
             
-        except AttributeError:
+        except NoSuchElementException:
+            
+            try:
+                table = self.driver.find_element(By.ID, "productDetails_detailBullets_sections1")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                
+                details_dict = {}
+                for row in rows:
+                    keyEl, valueEl = row.find_elements(By.XPATH, "*")
+                    
+                    key = keyEl.text
+                    if key in self.PRODUCT_DETAILS_MAP:
+                        col = self.PRODUCT_DETAILS_MAP[key]
+                        
+                        if key != "Best Sellers Rank":
+                            details_dict[col] = valueEl.text
+                        else:
+                            ranking_table = valueEl.text.split(r"\n")
+                            ranking_str = [rank for rank in ranking_table if "Health and Household" in rank]
+                            
+                            if ranking_str:
+                                ranking_str = ranking_str[0]
+                                rank_str = next(value for value in ranking_str.split() if value[0] == "#")
+                                rank_str = rank_str.replace(",", "")
+                                rank = int(rank_str[1:])
+                        
+                                details_dict['rank'] = rank
+                        
+                return details_dict
+                
+            except:
+                return {}
+        
+        except:
             return {}
 
     def _parseImportantInfo_(self) -> Dict[str, str]:
@@ -208,15 +245,88 @@ class AmazonWebInstance:
             
             
         
-        except AttributeError:
+        except:
             return {}
+
+    def _parseRatings_(self) -> Dict[str, str|int]:
+
+        try:
+            ratings_dict = {}
+            # Ratings parse
+            avgRatingEl = self.driver.find_element(By.ID, "acrPopover")
+            avgRating = avgRatingEl.get_attribute("title").split()[0]
+            avgRating = float(avgRating)
+
+            numRatingsEl = self.driver.find_element(By.ID, "acrCustomerReviewText")
+            numRatings_str = numRatingsEl.text
+            numRatings_str = numRatings_str.split()[0]
+            numRatings_str = numRatings_str.replace(",", "")
+            numRatings = int(numRatings_str)
+            
+            ratings_dict["rating"] = avgRating
+            ratings_dict['numRatings'] = numRatings
+            
+            return ratings_dict
+        except:
+            return {}
+
+    def getSearchProductLinks(self, term: str, maxResults: Optional[int]=None, page: Optional[int]=1) -> List[str]:
+        
+        search_link = f"{self.BASE_URL}/s?k={term}&i=hpc&crid=31LYPP9IF70TO&page={page}"
+        
+        self.driver.get(search_link)
+        
+        links = self.driver.find_elements(By.CSS_SELECTOR, "a[class*='a-link-normal s-no-outline']")
+        
+        ret_links = []
+        
+        for link in links:
+            href = link.get_dom_attribute("href")
+            
+            is_amazon_page = href[0:4] != "http"
+            sponsored_product = "gp/slredirect" in href
+            
+            if is_amazon_page and not sponsored_product:
+                cleaned_href = href.rsplit("/ref=", 1)[0]
+                
+                is_full_link = "www.amazon.com" in cleaned_href
+                
+                if is_full_link:
+                    ret_links.append(cleaned_href)
+                else:
+                    ret_links.append(f"{self.BASE_URL}{cleaned_href}")
+                    
+        unique_links = np.unique(ret_links)
+        
+        if max_links is not None:
+            unique_links = unique_links[:max_links]
+        
+        return unique_links.tolist()
 
 
 
 if __name__ == "__main__":    
     with AmazonWebInstance() as amazonUS:
         
-        url = "https://www.amazon.com/Banyan-Botanicals-Triphala-Detoxification-Rejuvenation/dp/B000Q454D8?th=1"
-        data = amazonUS.parsePage(url, 'Triphala')
+        # url = "https://www.amazon.com/Banyan-Botanicals-Triphala-Detoxification-Rejuvenation/dp/B000Q454D8?th=1"
+        searchTerm = "Neem"
+        productNo = 50
         
-        print(data.to_dict())
+        searchLinks = []
+        
+        i = 1
+        while len(searchLinks) < productNo:
+            
+            max_links = productNo - len(searchLinks)
+            newLinks = amazonUS.getSearchProductLinks(searchTerm, page=i, maxResults=max_links)
+            
+            searchLinks.extend(newLinks)
+            i += 1
+                
+            # print(len(searchLinks))
+        # amazonUS.driver.install_addon("noscript-11.4.14.xpi", temporary=True)
+        
+        data = [ amazonUS.parsePage(link, searchTerm) for link in tqdm.tqdm(searchLinks, desc="Processing webpages", unit="webpage")]
+            
+    data = convertDataToDataframe(data)
+    data.to_csv("output.csv", index=False, sep=";", quotechar="|")
